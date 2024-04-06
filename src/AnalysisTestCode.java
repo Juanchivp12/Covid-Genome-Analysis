ea2550bafa835baf06eef682f7168ebcf6569a3a
Juanchivp12 <Juanvillegasse@gmail.com>
2024-04-06 00:33:52 -0500

    added array of files
    I havent really read the assignment but

--- src/AnalysisTestCode.java
+++ src/AnalysisTestCode.java
@@ -1,10 +1,16 @@
 // file for code testing
 // Not sure how many class files we need to make yet
+import java.io.*;
+import java.util.Scanner;
 
 public class AnalysisTestCode
 {
-    public static void main(String[] args) 
+    public static void main(String[] args) throws IOException
     {
-        // testing
+        // Maybe make an array of files containing all 3 csv's?
+        File[] files = new File[3];
+        files[0] = new File("covidSequenceRF1.csv");
+        files[1] = new File("covidSequenceRF2.csv");
+        files[2] = new File("covidSequenceRF3.csv");
     }
 }

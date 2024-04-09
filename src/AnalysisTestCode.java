ee1460cf173150fee7192566d1012eaea6e67d34
Juanchivp12 <Juanvillegasse@gmail.com>
2024-04-09 17:30:18 -0500

    fixed a little
    Added printwriter and fixed the Scanner a little

--- src/AnalysisTestCode.java
+++ src/AnalysisTestCode.java
@@ -6,16 +6,18 @@ import java.util.Scanner;
 public class AnalysisTestCode
 {
     public static void main(String[] args) throws IOException
-    {
-        // Maybe make an array of files containing all 3 csv's? not sure
+    {   
+        Scanner kbd = new Scanner(System.in);
         File[] files = new File[3];
+        PrintWriter outFile = new PrintWriter("OutputFile.txt");
+
+        // Maybe make an array of files containing all 3 csv's? not sure
         files[0] = new File("covidSequenceRF1.csv");
         files[1] = new File("covidSequenceRF2.csv");
         files[2] = new File("covidSequenceRF3.csv");
-        // Got it to work heck yeah
-        Scanner kbd = new Scanner(System.in);
-        System.out.println("Enter the genome sequence:");
-        String genome = scanner.nextLine().toUpperCase(); // something like this is going to be needed to get user input
+        
+        System.out.print("Enter the genome sequence:");
+        String genomeSequence = kbd.nextLine().toUpperCase(); // something like this is going to be needed to get user input
     }
 
 }

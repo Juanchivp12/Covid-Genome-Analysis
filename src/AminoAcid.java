346c3366f5ea47a8369321fc636725f4465219b7
zndrtshmn <zndrtshmn@gmail.com>
2024-04-10 22:56:26 -0500

    Filled out AminoAcid and Gene classes
    I added the attributes, constructors, setters, getters, toString, equals, and clone methods for both the AminoAcid and Gene classes.

--- src/AminoAcid.java
+++ src/AminoAcid.java
@@ -1,16 +1,68 @@
-public class AminoAcid 
+import java.util.ArrayList;
+
+public class AminoAcid
 {
-    private String fullName;
-    private char abbreviation;
-    
-    public AminoAcid(String fullName, char abbreviation)
+    private String fullName = "";
+    private String abbreviation = "";
+    private ArrayList<String> codons = new ArrayList<String>();
+
+    // CONSTRUCTORS
+    public AminoAcid() {}
+
+    public AminoAcid(String fullName, String abbreviation, ArrayList<String> codons)
     {
         this.fullName = fullName;
         this.abbreviation = abbreviation;
+        this.codons = codons;
     }
-    // Constructors
 
-    // methods
+    // SETTERS
+    public void setFullName(String fullName)
+    {
+        this.fullName = fullName;
+    }
 
-    // Can't forget equals, toString and clone methods
+    public void setAbbreviation(String abbreviation)
+    {
+        this.abbreviation = abbreviation;
+    }
+
+    public void setCodons(ArrayList<String> codons)
+    {
+        this.codons = codons;
+    }
+
+    // GETTERS
+    public String getFullName()
+    {
+        return fullName;
+    }
+
+    public String getAbbreviation()
+    {
+        return abbreviation;
+    }
+
+    public ArrayList<String> getCodons()
+    {
+        return codons;
+    }
+
+    // toString, equals, and clone methods
+    public String toString()
+    {
+        return "";
+    }
+
+    public boolean equals(AminoAcid aminoAcidTest)
+    {
+        return this.fullName == aminoAcidTest.fullName
+                && this.abbreviation == aminoAcidTest.abbreviation
+                && this.codons == aminoAcidTest.codons;
+    }
+
+    public AminoAcid clone()
+    {
+        return new AminoAcid(fullName, abbreviation, codons);
+    }
 }
--- src/Gene.java
+++ src/Gene.java
@@ -1,4 +1,79 @@
-public class Gene 
+import java.util.ArrayList;
+
+public class Gene
 {
-    
+    private String aminoAcidSequence= "";
+    private int startPosition = 0;
+    private int endPosition = 0;
+    private int geneLength = 0;
+
+    // CONSTRUCTORS
+    public Gene() {}
+
+    public Gene(String aminoAcidSequence, int startPosition, int endPosition, int geneLength)
+    {
+        this.aminoAcidSequence = aminoAcidSequence;
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+        this.geneLength = geneLength;
+    }
+
+    // SETTERS
+    public void setAminoAcidSequence(String aminoAcidSequence)
+    {
+        this.aminoAcidSequence = aminoAcidSequence;
+    }
+
+    public void setStartPosition(int startPosition)
+    {
+        this.startPosition = startPosition;
+    }
+
+    public void setEndPosition(int endPosition)
+    {
+        this.endPosition = endPosition;
+    }
+
+    public void setGeneLength(int geneLength)
+    {
+        this.geneLength = geneLength;
+    }
+
+    // GETTERS
+    public String getAminoAcidSequence()
+    {
+        return aminoAcidSequence;
+    }
+
+    public int getStartPosition()
+    {
+        return startPosition;
+    }
+
+    public int getEndPosition()
+    {
+        return endPosition;
+    }
+
+    public int getGeneLength()
+    {
+        return geneLength;
+    }
+
+    // toString, equals, and clone methods
+    public String toString()
+    {
+        return "";
+    }
+
+    public boolean equals(Gene geneTest)
+    {
+        return this.aminoAcidSequence == geneTest.aminoAcidSequence && this.startPosition == geneTest.startPosition
+                && this.endPosition == geneTest.endPosition && this.geneLength == geneTest.geneLength;
+    }
+
+    public Gene clone()
+    {
+        return new Gene(aminoAcidSequence, startPosition, endPosition, geneLength);
+    }
 }

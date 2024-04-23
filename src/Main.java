bd8fde4a10a77132ed753e8fcbc9eb3601912cd1
zndrtshmn <zndrtshmn@gmail.com>
2024-04-22 20:44:35 -0500

    Polishing done, please check
    Check java docs, AminoAcid and Gene classes, comment any important lines explaining what it does, and any clarification that may be needed

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main
     public static void main(String[] args) throws IOException
     {
         Scanner kbd = new Scanner(System.in);
-        String RFFile = "";
+        String readingFrameFile = "";
 
         // User chooses file
         System.out.println("****Covid-19 Genome Analysis****");
@@ -23,18 +23,18 @@ public class Main
         int readingFrame = kbd.nextInt();
         System.out.println();
 
-        RFFile = switch (readingFrame)
+        readingFrameFile = switch (readingFrame)
         {
             case 1 -> "covidSequenceRF1.csv";
             case 2 -> "covidSequenceRF2.csv";
             case 3 -> "covidSequenceRF3.csv";
-            default -> RFFile;
+            default -> readingFrameFile;
         };
 
-        // User chooses action
+        // User chooses an analysis
         System.out.println("Which action would you like to perform?");
-        System.out.println("1. Gene Analysis Report");
-        System.out.print("2. Codon Bias Analysis ");
+        System.out.println(" 1. Gene Analysis Report");
+        System.out.print(" 2. Codon Bias Analysis ");
 
         int analysisChoice = kbd.nextInt();
         System.out.println();
@@ -42,15 +42,15 @@ public class Main
 
         if (analysisChoice == 2)
         {
-            System.out.println("Which would you like to do?");
-            System.out.println("1. Complete Amino Acid Codon Bias Report");
-            System.out.print("2. Individual Amino Acid Codon Bias Analysis ");
+            System.out.println("Which of these would you like to do?");
+            System.out.println(" 1. Complete Amino Acid Codon Bias Report");
+            System.out.print(" 2. Individual Amino Acid Codon Bias Analysis ");
             codonBiasChoice = kbd.nextInt();
             System.out.println();
         }
 
         // Read in file and tokenize the string into codons
-        File file = new File(RFFile);
+        File file = new File(readingFrameFile);
         Scanner infile = new Scanner(file);
         String str = "";
 
@@ -68,14 +68,13 @@ public class Main
             readTokens(geneSequences, str);
 
             // Prints gene information
-            printGenomeSequence(geneSequences, RFFile);
+            printGenomeSequence(geneSequences, readingFrameFile);
 
         }
         else if (analysisChoice == 2 && codonBiasChoice == 1)
         {
-            String completeCodonBiasAnalysis = "completeCodonBiasAnalysis.txt";
-            PrintWriter outfile = new PrintWriter(completeCodonBiasAnalysis);
-
+            String completeCodonBiasAnalysisFile = "completeCodonBiasAnalysis.txt";
+            PrintWriter outfile = new PrintWriter(completeCodonBiasAnalysisFile);
             outfile.println("*********** Complete Codon Bias Report **********\n");
 
             ArrayList<AminoAcid> aminoAcidList = readFromAminoAcidTable();
@@ -83,168 +82,19 @@ public class Main
             for (AminoAcid aminoAcid : aminoAcidList)
             {
                 outfile.println(aminoAcid.toString());
-                codonBiasAnalysis(RFFile, aminoAcid.getCodons(), codonBiasChoice, completeCodonBiasAnalysis, outfile);
+                codonBiasAnalysis(readingFrameFile, aminoAcid.getCodons(), codonBiasChoice, outfile);
                 outfile.println();
             }
             outfile.close();
-            System.out.println("\nCodon bias analysis has been written to " + completeCodonBiasAnalysis);
+            System.out.println("\nCodon bias analysis has been written to " + completeCodonBiasAnalysisFile);
         }
         else if (analysisChoice == 2 && codonBiasChoice == 2)
         {
-            lookForAcidFromList(RFFile, codonBiasChoice);
+            lookForAcidFromList(readingFrameFile, codonBiasChoice);
         }
         else
         {
             System.out.println("At least one invalid choice was made.");
-        }
-    }
-
-    /**
-     * This method asks the user to enter a letter and prints out the amino acid that contains that letter
-     * @param RFFile the reading frame file from which to read the amino acid table
-     * @param choiceTwo the choice made by the user
-     * @throws IOException if an I/O error occurs
-     */
-    public static void lookForAcidFromList(String RFFile, int choiceTwo) throws IOException
-    {
-        ArrayList<AminoAcid> aminoAcidList = readFromAminoAcidTable();
-        Scanner kbd = new Scanner(System.in);
-        String aminoAcidLetter;
-
-        do
-        {
-        System.out.print("Which Amino Acid would you like to see? (Enter its one letter abbreviation (Type -1 to quit)) ");
-        aminoAcidLetter = kbd.nextLine().toUpperCase();
-        System.out.println();
-
-        for (AminoAcid aminoAcid : aminoAcidList)
-        {
-            if (aminoAcidLetter.equals(aminoAcid.getAbbreviation()))
-            {
-                System.out.println(aminoAcid);
-                codonBiasAnalysis(RFFile, aminoAcid.getCodons(), choiceTwo, null, null);
-            }
-        }
-        System.out.println();
-        } while (!aminoAcidLetter.equals("-1"));
-    }
-
-    /**
-     * This method makes an array of AminoAcids using the aminoAcidTable file
-     * @return the array list of amino acids
-     * @throws IOException if an I/O error occurs
-     */
-    public static ArrayList<AminoAcid> readFromAminoAcidTable() throws IOException
-    {
-        File file = new File("aminoAcidTable.csv");
-        Scanner inFile = new Scanner(file);
-
-        // Skip first line of the file
-        inFile.nextLine();
-
-        ArrayList<AminoAcid> acidList = new ArrayList<>();
-
-        while (inFile.hasNext())
-        {
-            // Read items from file and extract tokens
-            String str = inFile.nextLine();
-            String[] tokens = str.split(",");
-
-            // Assign variables the values of tokens at specific positions
-            String name = tokens[0];
-            String abbreviation = tokens[2];
-
-            // Make array list for codons and read them from file and add them to it
-            ArrayList<String> codons = new ArrayList<>();
-            for (int i = 3; i < tokens.length; i++) {
-                codons.add(tokens[i]);
-            }
-
-            // Make an instance of the AminoAcid class and add information from aminoAcidTable file
-            acidList.add(new AminoAcid(name, abbreviation, codons));
-        }
-        inFile.close();
-        return acidList;
-    }
-
-    /**
-     * This method reads the codons from a file chosen by the user and adds each codon into an array list
-     * @param RFFile the reading frame file
-     * @return codonsFromFile the array list of codons
-     * @throws IOException if an I/O error occurs
-     */
-    public static ArrayList<String> readCodonsFromFile(String RFFile) throws IOException
-    {
-        ArrayList<String> codonsFromFile = new ArrayList<>();
-        File file = new File(RFFile);
-        Scanner inFile = new Scanner(file);
-
-        while (inFile.hasNext())
-        {
-            String str = inFile.nextLine();
-            String[] tokens = str.split(",");
-
-            for (String s : tokens)
-            {
-                codonsFromFile.add(s);
-            }
-        }
-        inFile.close();
-
-        return codonsFromFile;
-    }
-
-    /**
-     * This method performs codon bias analysis and writes the output to a file.
-     * @param RFFile the reading frame file from which to read codons
-     * @param codons the list of codons
-     * @param choiceTwo the choice made by the user
-     * @param outputFileName the name of the output file
-     * @throws IOException if an I/O error occurs
-     */
-    public static void codonBiasAnalysis(String RFFile, ArrayList<String> codons, int choiceTwo, String outputFileName, PrintWriter writer) throws IOException
-    {
-        ArrayList<String> codonsFromFile = readCodonsFromFile(RFFile);
-
-        // Initialize arrays to store counts and percentages for each codon
-        int[] codonCounts = new int[codons.size()];
-        double[] codonPercentages = new double[codons.size()];
-
-        // Initialize a variable to count the total number of codons
-        int totalCount = 0;
-
-        // Count occurrences of each codon
-        if (choiceTwo == 1)
-        {
-            for (String codon : codonsFromFile)
-            {
-                int index = codons.indexOf(codon);
-                if (index != -1)
-                {
-                    codonCounts[index]++;
-                    totalCount++;
-                }
-            }
-            // Output the complete codon bias report and iterate over each codon for the current amino acid
-            for (int i = 0; i < codons.size(); i++)
-            {
-                int count = codonCounts[i];
-                double percentage = ((double) count / totalCount) * 100;
-                codonPercentages[i] = percentage;
-                writer.printf("%s: %d %.2f%%\n", codons.get(i), count, percentage);
-            }
-        }
-        else
-        {
-            for (String codon : codonsFromFile)
-            {
-                int index = codons.indexOf(codon);
-                if (index != -1)
-                {
-                    codonCounts[index]++;
-                    totalCount++;
-                }
-            }
         }
     }
 
@@ -285,7 +135,8 @@ public class Main
                 {
                     // Calculates gene length, start nucleotide, and stop nucleotide
                     int geneLength = endPosition - startPosition + 1;
-                    int startNucleotide = startPosition * 3, endNucleotide = startNucleotide + (geneLength * 3);
+                    int startNucleotide = startPosition * 3;
+                    int endNucleotide = startNucleotide + (geneLength * 3);
 
                     // Adds a new gene of the previously found information into the first array list made
                     geneSequences.add(new Gene(aminoAcidSequence, startNucleotide, endNucleotide, geneLength));
@@ -297,25 +148,171 @@ public class Main
     /**
      * This method prints the genome sequence.
      * @param geneSequences the list of gene sequences
+     * @param readingFrameFile the reading frame file
      */
-    public static void printGenomeSequence(ArrayList<Gene> geneSequences, String RFFile) throws IOException
+    public static void printGenomeSequence(ArrayList<Gene> geneSequences, String readingFrameFile) throws IOException
     {
-        String geneSequence = "geneSequence.txt";
+        String geneSequenceFile = "geneSequence.txt";
+        PrintWriter outfile = new PrintWriter(geneSequenceFile);
+        outfile.println("** Gene analysis for file: " + readingFrameFile + " **\n");
 
-        PrintWriter outfile = new PrintWriter(geneSequence);
-        outfile.println("** Gene analysis for file: " + RFFile + " **\n");
-
-        int i = 1;
+        int geneIndex = 1;
         for (Gene g : geneSequences)
         {
-            outfile.println("Gene " + i + " (" + g.getGeneLength() + ") :");
+            outfile.println("Gene " + geneIndex + " (" + g.getGeneLength() + ") :");
             outfile.println(g.getStartNucleotide() + ".." + g.getEndNucleotide());
             outfile.print("Sequence: ");
             outfile.println(g.getAminoAcidSequence());
             outfile.println();
-            i++;
+            geneIndex++;
         }
         outfile.close();
-        System.out.println("Genome sequence information has been written to " + geneSequence);
+        System.out.println("Genome sequence information has been written to " + geneSequenceFile);
+    }
+
+    /**
+     * This method makes an array of AminoAcids using the aminoAcidTable file
+     * @return the array list of amino acids
+     * @throws IOException if an I/O error occurs
+     */
+    public static ArrayList<AminoAcid> readFromAminoAcidTable() throws IOException
+    {
+        File file = new File("aminoAcidTable.csv");
+        Scanner infile = new Scanner(file);
+
+        // Skip first line of the file
+        infile.nextLine();
+
+        ArrayList<AminoAcid> acidList = new ArrayList<>();
+
+        while (infile.hasNext())
+        {
+            // Read items from file and extract tokens
+            String str = infile.nextLine();
+            String[] tokens = str.split(",");
+
+            // Assign variables the values of tokens at specific positions
+            String name = tokens[0];
+            String abbreviation = tokens[2];
+
+            // Make an array list for codons and read them from file and add them to it
+            ArrayList<String> codons = new ArrayList<>();
+            for (int i = 3; i < tokens.length; i++)
+            {
+                codons.add(tokens[i]);
+            }
+
+            // Make an instance of the AminoAcid class and add information from aminoAcidTable file
+            acidList.add(new AminoAcid(name, abbreviation, codons));
+        }
+        infile.close();
+        return acidList;
+    }
+
+    /**
+     * This method asks the user to enter a letter and prints out the amino acid that contains that letter
+     * @param readingFrameFile the reading frame file from which to read the amino acid table
+     * @param codonBiasChoice the choice made by the user
+     * @throws IOException if an I/O error occurs
+     */
+    public static void lookForAcidFromList(String readingFrameFile, int codonBiasChoice) throws IOException
+    {
+        ArrayList<AminoAcid> aminoAcidList = readFromAminoAcidTable();
+        Scanner kbd = new Scanner(System.in);
+        String aminoAcidLetter;
+
+        do
+        {
+        System.out.print("Which Amino Acid would you like to see? (Enter its one letter abbreviation (Type -1 to quit)) ");
+        aminoAcidLetter = kbd.nextLine().toUpperCase();
+        System.out.println();
+
+        for (AminoAcid aminoAcid : aminoAcidList)
+        {
+            if (aminoAcidLetter.equals(aminoAcid.getAbbreviation()))
+            {
+                System.out.println(aminoAcid);
+                codonBiasAnalysis(readingFrameFile, aminoAcid.getCodons(), codonBiasChoice, null);
+            }
+        }
+        System.out.println();
+        } while (!aminoAcidLetter.equals("-1"));
+    }
+
+    /**
+     * This method reads the codons from a file chosen by the user and adds each codon into an array list
+     * @param readingFrameFile the reading frame file
+     * @return codonsFromFile the array list of codons
+     * @throws IOException if an I/O error occurs
+     */
+    public static ArrayList<String> readCodonsFromFile(String readingFrameFile) throws IOException
+    {
+        ArrayList<String> codonsFromFile = new ArrayList<>();
+        File file = new File(readingFrameFile);
+        Scanner infile = new Scanner(file);
+
+        while (infile.hasNext())
+        {
+            String str = infile.nextLine();
+            String[] tokens = str.split(",");
+
+            for (String s : tokens)
+            {
+                codonsFromFile.add(s);
+            }
+        }
+        infile.close();
+
+        return codonsFromFile;
+    }
+
+    /**
+     * This method performs codon bias analysis and writes the output to a file.
+     * @param readingFrameFile the reading frame file
+     * @param codons the list of codons
+     * @param codonBiasChoice the choice made by the user
+     * @throws IOException if an I/O error occurs
+     */
+    public static void codonBiasAnalysis(String readingFrameFile, ArrayList<String> codons, int codonBiasChoice, PrintWriter outfile) throws IOException
+    {
+        ArrayList<String> codonsFromFile = readCodonsFromFile(readingFrameFile);
+
+        // Initialize arrays to store counts and percentages for each codon
+        int[] codonCounts = new int[codons.size()];
+        double[] codonPercentages = new double[codons.size()];
+
+        // Initialize a variable to count the total number of codons
+        int totalCount = 0;
+
+        // Count occurrences of each codon
+        if (codonBiasChoice == 1)
+        {
+            for (String codon : codonsFromFile)
+            {
+                int index = codons.indexOf(codon);
+                if (index != -1)
+                {
+                    codonCounts[index]++;
+                    totalCount++;
+                }
+            }
+            // Output the complete codon bias report and iterate over each codon for the current amino acid
+            for (int i = 0; i < codons.size(); i++)
+            {
+                int count = codonCounts[i];
+                double percentage = ((double) count / totalCount) * 100;
+                codonPercentages[i] = percentage;
+                outfile.printf("%s: %d %.2f%%\n", codons.get(i), count, percentage);
+            }
+        }
+        else
+        {
+            for (String codon : codonsFromFile)
+            {
+                int index = codons.indexOf(codon);
+                codonCounts[index]++;
+                totalCount++;
+            }
+        }
     }
 }
